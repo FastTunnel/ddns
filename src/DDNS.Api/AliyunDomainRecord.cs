ad2c1b84b0c3a092d350f817add1e4475c9df42f
SpringHgui <740360381@qq.com>
2020-04-14 13:08:11 +0800

    初始化项目

---
+++ src/DDNS.Api/AliyunDomainRecord.cs
@@ -0,0 +1,32 @@
+﻿using Aliyun.Acs.Alidns.Model.V20150109;
+using Aliyun.Acs.Core;
+using Aliyun.Acs.Core.Exceptions;
+using Aliyun.Acs.Core.Profile;
+using System;
+
+namespace DDNS.Api
+{
+    public class Aliyun : IDomainRecord
+    {
+        public void dd()
+        {
+            IClientProfile profile = DefaultProfile.GetProfile("cn-hangzhou", "<accessKeyId>", "<accessSecret>");
+            DefaultAcsClient client = new DefaultAcsClient(profile);
+
+            var request = new AddDomainRecordRequest();
+            try
+            {
+                var response = client.GetAcsResponse(request);
+                Console.WriteLine(System.Text.Encoding.Default.GetString(response.HttpResponse.Content));
+            }
+            catch (ServerException e)
+            {
+                Console.WriteLine(e);
+            }
+            catch (ClientException e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
---
+++ src/DDNS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DDNS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
